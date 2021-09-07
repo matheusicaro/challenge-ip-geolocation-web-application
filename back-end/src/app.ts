349d74343341007b1a99d03bc5c3f8ca3cc622db
Matheus Ícaro <matheusicaro2@hotmail.com>
2021-09-07 12:04:57 -0300

    back-end: Fixing lint settings, swagger contract and adding messages constant.

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -7,24 +7,26 @@ import swaggerUI from 'swagger-ui-express';
 import swaggerDocs from './swagger.json';
 
 import appRouters from './routes/app.routes';
-import environment from './config/environment';
+import env from './config/environment';
 
 const corsOptions: cors.CorsOptions = {
-  allowedHeaders: ['Authorization', 'Content-Type'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+  allowedHeaders: ['Content-Type'],
+  methods: ['GET', 'POST']
 };
 
 const app = express();
 
-app.set('env', environment.NODE_ENV);
-app.set('port', environment.PORT);
-app.set('host', environment.HOST_NAME);
+swaggerDocs.host = env.NODE_ENV === 'production' ? `${env.HOST_NAME}` : `${env.HOST_NAME}:${env.PORT}`;
+
+app.set('env', env.NODE_ENV);
+app.set('port', env.PORT);
+app.set('host', env.HOST_NAME);
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan('common'));
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
-app.use(environment.BASE_PATH, appRouters);
+app.use(env.BASE_PATH, appRouters);
 
 export default app;
---
+++ back-end/src/constants/index.ts
@@ -0,0 +1,5 @@
+export const MESSAGES = {
+  INVALID_BODY: 'Missing or invalid body',
+  NOT_FOUND: 'Not found data for parameters informed',
+  INTERNAL_ERRO: 'Internal Error, Try again later'
+};
